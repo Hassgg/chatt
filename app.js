705b6c40dbea4ba6c88c0cf013658d4a8c74b7e8
Hassgg <87785406+Hassgg@users.noreply.github.com>
2023-04-10 23:33:33 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
 const express = require("express");
 const app = express();
+const openai = require("openai");
+const axios = require("axios");
 
-// define path to the HTML file
-const path = require("path");
-const htmlPath = path.join(__dirname, "index.html");
+openai.apiKey = "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";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// serve the HTML file at the root URL
-app.get("/", (req, res) => {
-  res.sendFile(htmlPath);
-});
-
-// handle chat API requests
 app.post("/api/chat", async (req, res) => {
-  // code for handling chat requests goes here
+  const message = req.body.message;
+
+  try {
+    // إجراء طلب إلى OpenAI API باستخدام axios
+    const openaiResponse = await axios.post(
+      "https://api.openai.com/v1/engines/davinci-codex/completions",
+      {
+        prompt: message,
+        max_tokens: 100,
+        n: 1,
+        stop: null,
+        temperature: 0.5,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${openai.apiKey}`,
+        },
+      }
+    );
+
+    // استخراج الاستجابة من البيانات المستلمة
+    const responseText = openaiResponse.data.choices[0].text.trim();
+
+    // إرسال الرد للعميل
+    res.json({ message: responseText });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "حدث خطأ أثناء معالجة طلبك" });
+  }
 });
 
-// start the server
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
